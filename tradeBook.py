ee516dd76920f7e91c654d86b868ed09d0ada2d5
Ravi <Ravi@Ravis-MacBook-Pro.local>
2014-10-04 18:02:36 -0400

    update TradeBook

--- tradeBook.py
+++ tradeBook.py
@@ -3,6 +3,34 @@
 import sys
 import pandas as pd
 
+class TradeBook:
+
+    def build(self, d):
+        trade = d[d[1] == 'P']
+        quote = d[d[1] == 'Q']
+        bid = quote[quote[4] == 'B'][[5,6,7]]
+        ask = quote[quote[4] == 'A'][[5,6,7]]
+        bid.columns = ['bprice', 'bsize', 'borders']
+        ask.columns = ['aprice', 'asize', 'aorders']
+
+        bid_ask = bid.join(ask, how="outer")
+        ba = bid_ask.fillna(method='ffill')
+
+        trade[4] = trade[4].astype(float)
+        t = trade.iloc[:,(3,4)]
+        t.columns = ['tprice', 'tq']
+        tgp = t.groupby(level=0)
+
+        tr = pd.DataFrame(tgp['tprice'].mean()).join(pd.DataFrame(tgp['tq'].sum()))
+
+        book = tr.join(ba, how="outer")
+        book['tprice'] = book['tprice'].fillna(0)
+        book['tq'] = book['tq'].fillna(0)
+        book = book.fillna(method='ffill')
+
+        return book
+
+
 if __name__ == '__main__':
 
     if (len(sys.argv) < 3):
@@ -12,27 +40,6 @@ if __name__ == '__main__':
     pd.options.mode.chained_assignment = None
 
     d = pd.read_csv(sys.argv[1], index_col=0, header=None)
-
-    trade = d[d[1] == 'P']
-    quote = d[d[1] == 'Q']
-    bid = quote[quote[4] == 'B'][[5,6,7]]
-    ask = quote[quote[4] == 'A'][[5,6,7]]
-    bid.columns = ['bprice', 'bsize', 'borders']
-    ask.columns = ['aprice', 'asize', 'aorders']
-
-    bid_ask = bid.join(ask, how="outer")
-    ba = bid_ask.fillna(method='ffill')
-
-    trade[4] = trade[4].astype(float)
-    t = trade.iloc[:,(3,4)]
-    t.columns = ['tprice', 'tq']
-    tgp = t.groupby(level=0)
-
-    tr = pd.DataFrame(tgp['tprice'].mean()).join(pd.DataFrame(tgp['tq'].sum()))
-
-    book = tr.join(ba, how="outer")
-    book['tprice'] = book['tprice'].fillna(0)
-    book['tq'] = book['tq'].fillna(0)
-    book = book.fillna(method='ffill')
-
+    tb = TradeBook()
+    book = tb.build(d)
     book.to_csv(sys.argv[2])

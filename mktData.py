63088f27285f082dc3177507dafe490d7a6d5844
Ravi <Ravi@Ravis-MacBook-Pro.local>
2014-10-04 13:12:59 -0400

    added mktData.py to download yahoo finance data
    options and underlyinh

---
+++ mktData.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import numpy as np
+import pandas as pd
+import sys
+import datetime
+
+from pandas.io.data import DataReader
+from pandas.io.data import Options
+
+np.set_printoptions(precision=4, suppress=True, linewidth=2000)
+pd.set_option('display.width', 2000)
+pd.set_option('max.columns', 50)
+pd.set_option('precision', 4)
+
+def get_underlying(sym, start_date):
+    now = datetime.datetime.now()
+    dr = DataReader(sym, 'yahoo', start=start_date)
+    now2 = datetime.datetime.now()
+    print 'get_underlying():', now2-now
+    return (now, dr)
+
+def get_options(sym, expiry):
+    opt = Options(sym, 'yahoo')
+    now = datetime.datetime.now()
+    opt_data = opt.get_options_data(expiry=expiry)
+    now2 = datetime.datetime.now()
+    opt_data2 = opt.get_near_stock_price(expiry=expiry, put=True, above_below=4)
+    now3 = datetime.datetime.now()
+    print 'get_options():', now2-now
+    print 'get_options():', now3-now2
+    return (now, opt_data2)
+
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+        print 'need symbol, usage:', sys.argv[0], '<sym> [start_date or expiration] [OPT]'
+        sys.exit(1)
+
+    sym = sys.argv[1]
+    today = datetime.date.today()
+    lastMonth = today - datetime.timedelta(days=30)
+    nextMonth = today + datetime.timedelta(days=30)
+
+    if (len(sys.argv) < 3):
+        start_date = lastMonth
+        expiry = nextMonth
+    else:
+        d = pd.to_datetime(sys.argv[2]).date()
+        start_date = d
+        expiry = d
+
+    if (len(sys.argv) < 4):
+        print 'fetch underlying from', start_date
+        data = get_underlying(sym, start_date)
+    else:
+        print 'fetch option', expiry
+        data = get_options(sym, expiry)
+
+    print data[0]
+    print data[1]

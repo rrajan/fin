e4ead55f29dd50b013644dbd30b6bef2a0530076
Ravi <Ravi@Ravis-MacBook-Pro.local>
2014-10-22 22:10:48 -0400

    fixed surface

--- mktData.py
+++ mktData.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import sys
 import datetime
+import urllib
+import time
 
 from pandas.io.data import DataReader
 from pandas.io.data import Options
@@ -13,12 +15,34 @@ pd.set_option('display.width', 2000)
 pd.set_option('max.columns', 50)
 pd.set_option('precision', 4)
 
+def get_quote(symbol):
+    data = []
+    url = 'http://finance.yahoo.com/d/quotes.csv?s='
+    #for s in symbols:
+    #    url += s+"+"
+    #url = url[0:-1]
+    url += symbol
+    url += "&f=sb3b2l1l"
+    now = datetime.datetime.now()
+    f = urllib.urlopen(url,proxies = {})
+    rows = f.read()
+    now2 = datetime.datetime.now()
+    print 'get_quote():', now2-now
+    values = [x for x in rows.split(',')]
+    #symbol = values[0][1:-1]
+    #bid = values[1]
+    #ask = values[2]
+    #last = values[3]
+    #data.append([symbol,bid,ask,last,values[4]])
+    #return data
+    return (now2, values)
+
 def get_underlying(sym, start_date):
     now = datetime.datetime.now()
     dr = DataReader(sym, 'yahoo', start=start_date)
     now2 = datetime.datetime.now()
     print 'get_underlying():', now2-now
-    return (now, dr)
+    return (now2, dr)
 
 def get_options(sym, expiry, above_below=0):
     opt = Options(sym, 'yahoo')
@@ -28,9 +52,17 @@ def get_options(sym, expiry, above_below=0):
         opt_data = opt.get_near_stock_price(expiry=expiry, put=True, above_below=above_below)
     now2 = datetime.datetime.now()
     print 'get_options():', now2-now
-    return (now, opt_data2)
+    return (now2, opt_data)
+
+def run():
+    while [True]:
+        print "Help"
+        time.sleep(10)
 
 if __name__ == '__main__':
+
+    run()
+
     if (len(sys.argv) < 2):
         print 'need symbol, usage:', sys.argv[0], '<sym> [start_date or expiration] [OPT]'
         sys.exit(1)
@@ -41,19 +73,17 @@ if __name__ == '__main__':
     nextMonth = today + datetime.timedelta(days=30)
 
     if (len(sys.argv) < 3):
-        start_date = lastMonth
-        expiry = nextMonth
+        data = get_quote(sym)
     else:
         d = pd.to_datetime(sys.argv[2]).date()
         start_date = d
         expiry = d
-
-    if (len(sys.argv) < 4):
-        print 'fetch underlying from', start_date
-        data = get_underlying(sym, start_date)
-    else:
-        print 'fetch option', expiry
-        data = get_options(sym, expiry)
+        if (len(sys.argv) < 4):
+            print 'fetch underlying from', start_date
+            data = get_underlying(sym, start_date)
+        else:
+            print 'fetch option', expiry
+            data = get_options(sym, expiry)
 
     print data[0]
     print data[1]
--- surface.py
+++ surface.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import datetime
+import sys
 
 import Amop
 from CalcIV import IV
@@ -23,19 +24,41 @@ def join_options(calls, puts):
     return st
 
 def ivs(st, S, r, t0):
+    st['Tau'] = -1
     st['IV'] = -1
     st['TV_c'] = -1
     st['TV_p'] = -1
     st = st.fillna(0)
     for i in st.index:
-        t = (i[1].date() - t0).days / 365.0
+        #t = (i[1].date() - t0).days / 365.0
+        t = (i[1] - t0).days / 365.0
         d = st.loc[i]
         iv = IV(S, i[2],d[0], d[1], d[2], d[3], r, t)
         iv.calc()
-        d[4] = iv.sigma
-        d[5] = iv.Ctv
-        d[6] = iv.Ptv
+        d[4] = t
+        d[5] = iv.sigma
+        d[6] = iv.Ctv
+        d[7] = iv.Ptv
         st.loc[i] = d
 
-    out = st[['Bid_c', 'TV_c', 'Ask_c', 'IV', 'Bid_p', 'TV_p', 'Ask_p']]
+    out = st[['Bid_c', 'TV_c', 'Ask_c', 'IV', 'Tau', 'Bid_p', 'TV_p', 'Ask_p']]
     return out
+
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+        print "need input"
+        sys.exit(1)
+
+    mktData = pd.read_csv(sys.argv[1])
+    S = mktData['Underlying_Price'][0]
+    t0 = pd.to_datetime(mktData['Quote_Time'][0])
+    r = 0.01
+    #print mktData
+    calls = mktData[mktData['Type'] == 'call']
+    puts = mktData[mktData['Type'] == 'put']
+
+    opt = join_options(calls, puts)
+    opt_ivs = ivs(opt, S, r, t0)
+
+    print 'writing to - ', sys.argv[1] + '.ivs'
+    opt_ivs.to_csv(sys.argv[1] + '.ivs', float_format='%.4f')
